45b0963ddbbba54991ca8cf6100751b360e2957e
Pleho <tpleho1@etf.unsa.ba>
2021-03-30 15:22:57 +0200

    tutorijal 3

---
+++ src/sample/Artikal.java
@@ -0,0 +1,28 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Artikal {
+    private String šifra;
+    private String naziv;
+    private double cijena;
+    public String šifranazivcijena;
+
+    public boolean equals(Object o) {
+        return true;
+    }
+
+    Artikal artikal = (Artikal) o;
+
+
+    public String toString() {
+
+        return null;
+    }
+
+    public static ArrayList izbaciDuplikate() {
+
+
+        return null;
+    }
+}
---
+++ src/sample/Racun.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Racun {
+
+    public void dodajStavku(){
+        public String artikal;
+        public int količina;
+
+    }
+
+
+    public double ukupanIznos(){
+
+    }
+
+}
